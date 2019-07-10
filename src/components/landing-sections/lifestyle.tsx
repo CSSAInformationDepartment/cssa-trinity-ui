1f3549785ca35a68f647e7423119a0cac961eaf4
Mingyu <mingyusu9841@gmail.com>
2019-07-10 17:34:43 +1000

    Add a general structure for the lifestyle section

--- src/components/landing-sections/lifestyle.tsx
+++ src/components/landing-sections/lifestyle.tsx
@@ -8,7 +8,7 @@ export class LifestyleSection extends Component {
                 id="lifestyle" 
                 title= "墨城"
                 bgColor= "purple">
-                <p>
+                {/* <p>
                     负责人：Nate、Jason
                     页面内容摘要（发布前删除）：
                     <br></br>
@@ -19,7 +19,74 @@ export class LifestyleSection extends Component {
                     3. 折扣商家信息
                     <br></br>
                     4. 优惠券
-                </p>
+                </p> */}
+
+                <div className="container">
+                    <div className="row mb-2">
+                        <div className="col-sm-6">
+                            <div className="row mb-2">
+                                <div className="col-6">
+                                    <div className="tile-blue">
+                                        <div className="card">
+                                            <img src={"/favicon.ico"}/>
+                                            <div className="card-img-overlay">
+                                                墨城攻略入口
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-6">
+                                    <div className="tile-blue">
+                                        <div className="card">
+                                            <img src={"/favicon.ico"}/>
+                                            <div className="card-img-overlay">
+                                                墨城攻略入口
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="row">
+                                <div className="col-6">
+                                    <div className="tile-blue">
+                                        <div className="card">
+                                            <img src={"/favicon.ico"}/>
+                                            <div className="card-img-overlay">
+                                                墨城攻略入口
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-6">
+                                    <div className="tile-blue">
+                                        更多
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </div>
+                        <div className="col-sm-6">
+                            <div className="tile-blue">
+                                美食地图
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="row mb-2">
+                        <div className="col-sm-6">
+                            <div className="tile-blue">
+                                折扣商家信息
+                            </div>
+                        </div>
+                        <div className="col-sm-6">
+                            <div className="tile-blue">
+                                优惠券
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </BaseSection>
         )
     }
