f5c41035e523eee3cc5e1e38ac888c36c8fc0c19
Yifei Zhu <40694262+ChloeZzzzz@users.noreply.github.com>
2019-07-11 16:40:08 +1000

    change the card block into card deck
    - implementing card deck

--- src/components/landing-sections/career.tsx
+++ src/components/landing-sections/career.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import BaseSection from  './base';
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
+import { CardDeck } from 'react-bootstrap';
 
 export class CareerSection extends Component {
     render() {
@@ -11,7 +12,7 @@ export class CareerSection extends Component {
                 title= "职场"
                 bgColor= "info">
                 <p>
-                    负责人：Nate、Jason
+                    负责人：Judy, Chloe
                     <br></br>
                     页面内容摘要（发布前删除）：
                     <br></br>
@@ -20,17 +21,57 @@ export class CareerSection extends Component {
                     2. 求职相关信息
 
                 </p>    
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
+                <CardDeck>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Job title</Card.Title>
+                            <Card.Text>
+                             - Company<br></br>
+                             - The brief introduction of the job
+                            </Card.Text>
                         <Button variant="primary">Learn more</Button>
-                    </Card.Body>
-                </Card>
+                        </Card.Body>
+                    </Card>
+
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Job title</Card.Title>
+                            <Card.Text>
+                             - Company <br></br>
+                             - The brief introduction of the job
+                            </Card.Text>
+                        <Button variant="primary">Learn more</Button>
+                        </Card.Body>
+                    </Card>
+
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Job title</Card.Title>
+                            <Card.Text>
+                             - Company<br></br>
+                             - The brief introduction of the job
+                            </Card.Text>
+                        <Button variant="primary">Learn more</Button>
+                        </Card.Body>
+                    </Card>
+
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Job title</Card.Title>
+                            <Card.Text>
+                             - Company<br></br>
+                             - The brief introduction of the job
+                            </Card.Text>
+                        <Button variant="primary">Learn more</Button>
+                        </Card.Body>
+                    </Card>
+
+                </CardDeck>
+
             </BaseSection>
         )
     }
