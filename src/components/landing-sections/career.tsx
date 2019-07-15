5fc49a453692a14a5dd275da9981998c418f0eb3
abeishiwo <45863441+abeishiwo@users.noreply.github.com>
2019-07-15 17:38:10 +1000

    finish merge

--- src/components/landing-sections/career.tsx
+++ src/components/landing-sections/career.tsx
@@ -40,40 +40,16 @@ export class CareerSection extends Component {
                                 CareerWebsite=""
                                 CompanyLogo=""/>
 
-<<<<<<< HEAD
-                            <Card style={{ width: '18rem' }}>
-                                <Card.Img variant="top" src="holder.js/100px180" />
-                                <Card.Header>
-                                    <h3>Job title</h3>
-                                    <Card.Text>Company</Card.Text>
-                                </Card.Header>
-                                <Card.Body>
-                                    <Card.Text>
-                                     - The brief introduction of the job
-                                     放轻松。大家都希望和朋友自在地相处，随时能开开玩笑，这样很有安全感。要是你想表现得好相处，就要放轻松，让大家也觉得轻松愉快。
-赞美别人。表示你的关心和专注。
-多笑。使用大方的肢体动作，让别人感觉你放得开。
-尽量放轻松。假如你紧张，你的朋友也会紧张，所以要放轻松。
-                                    </Card.Text>
-                                    <Button variant="primary">Learn more</Button>
-                                </Card.Body>
-                            </Card>
-=======
                             <CareerInfo
                                 CareerWebsite=""
                                 CompanyLogo=""/>
->>>>>>> 873e8064d46aa7fcd5e646713b68ff8bde4ebd47
 
                         </CardDeck>
 
                     </Carousel.Item>
                 </Carousel>
                 {/*browse more jobs button at the bottom of the carousel*/}
-<<<<<<< HEAD
-                <Button href = "#" variant = "link">查看更多</Button>
-=======
                 <Button href = "" variant = "link">查看更多</Button>
->>>>>>> 873e8064d46aa7fcd5e646713b68ff8bde4ebd47
             </Col>
             
             <Col>
@@ -83,17 +59,6 @@ export class CareerSection extends Component {
                         <h5>相关信息</h5>
                     </Card.Header>
                     <Card.Body>
-<<<<<<< HEAD
-                        <p>
-                        关注你的朋友。不玩手机，和朋友进行眼神交流，让他们感觉受重视。假如你分神，看起来心不在焉，那你身边的人就无法放松，也无法觉得有趣了。
-用眼神表示肯定。不要让别人觉得你自视清高或在评判他们，不然他们在你身边会很拘谨。关注你的朋友。不玩手机，和朋友进行眼神交流，让他们感觉受重视。假如你分神，看起来心不在焉，那你身边的人就无法放松，也无法觉得有趣了。
-用眼神表示肯定。不要让别人觉得你自视清高或在评判他们，不然他们在你身边会很拘谨。关注你的朋友。不玩手机，和朋友进行眼神交流，让他们感觉受重视。假如你分神，看起来心不在焉，那你身边的人就无法放松，也无法觉得有趣了。
-用眼神表示肯定。不要让别人觉得你自视清高或在评判他们，不然他们在你身边会很拘谨。关注你的朋友。不玩手机，和朋友进行眼神交流，让他们感觉受重视。假如你分神，看起来心不在焉，那你身边的人就无法放松，也无法觉得有趣了。
-用眼神表示肯定。不要让别人觉得你自视清高或在评判他们，不然他们在你身边会很拘谨。关注你的朋友。不玩手机，和朋友进行眼神交流，让他们感觉受重视。假如你分神，看起来心不在焉，那你身边的人就无法放松，也无法觉得有趣了。
-用眼神表示肯定。不要让别人觉得你自视清高或在评判他们，不然他们在你身边会很拘谨。                                  
-                        </p>
-=======
->>>>>>> 873e8064d46aa7fcd5e646713b68ff8bde4ebd47
                     </Card.Body>
                 </Card>
             </Col>
